c00528ebf28826fceb36218494c5ad0bf878a5f6
Vishnu <vm@ebizzinfotech.com>
2019-09-02 15:23:23 +0530

    version 4.2

---
+++ .build/x86_64-apple-macosx/debug/CNIOLinux.build/ifaddrs-android.c.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/ebitnhp-i1/Desktop/Vapor/firstMySQL/.build/checkouts/swift-nio/Sources/CNIOLinux/ifaddrs-android.c
---
+++ .build/x86_64-apple-macosx/debug/CNIOLinux.build/shim.c.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/ebitnhp-i1/Desktop/Vapor/firstMySQL/.build/checkouts/swift-nio/Sources/CNIOLinux/shim.c
